0d66f12f1dc77a89ee4be730062862312b6a91e7
Ričards <ricards.grinbergs@outlook.com>
2022-12-08 08:19:11 +0200

    Day 7 Part 2

--- 07/main.go
+++ 07/main.go
@@ -4,15 +4,29 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
+var input []string
 var sizes = make(map[string]int)
 var current = 0
 
 func main() {
-	one()
+	scanner := scanner("07/input.txt")
+	for scanner.Scan() {
+		line := scanner.Text()
+		input = append(input, line)
+	}
+
+	recursive("/")
+
+	// Solution 1
+	fmt.Println(getTotalSize())
+
+	// Solution 2
+	fmt.Println(getSmallestSize())
 }
 
 func scanner(input string) *bufio.Scanner {
@@ -24,22 +38,22 @@ func scanner(input string) *bufio.Scanner {
 	return bufio.NewScanner(file)
 }
 
-func recursive(path string, commands []string) {
+func recursive(path string) {
 	size := 0
-	for current < len(commands) {
-		line := commands[current]
-		if commands[current] == "$ cd .." {
-			break
-		}
-
+	for current < len(input) {
+		line := input[current]
 		if strings.HasPrefix(line, "dir") {
 			current++
 			continue
 		}
 		if strings.HasPrefix(line, "$ cd ") {
+			cd := strings.TrimPrefix(line, "$ cd ")
+			if cd == ".." {
+				break
+			}
 			current += 2
-			recursive(path+"/"+strings.TrimPrefix(line, "$ cd "), commands)
-			size += sizes[path+"/"+strings.TrimPrefix(line, "$ cd ")]
+			recursive(path + "/" + cd)
+			size += sizes[path+"/"+cd]
 		} else {
 			size += convert(strings.Split(line, " ")[0])
 		}
@@ -55,4 +69,37 @@ func convert(s string) int {
 	}
 
 	return i
+}
+
+func getSmallestSize() int {
+	free := 70000000 - sizes["/"]
+	need := 30000000 - free
+
+	var temp []int
+	for _, size := range sizes {
+		temp = append(temp, size)
+	}
+
+	result := 0
+
+	sort.Ints(temp)
+	for _, size := range temp {
+		if size >= need {
+			result = size
+			break
+		}
+	}
+
+	return result
+}
+
+func getTotalSize() int {
+	sum := 0
+	for _, size := range sizes {
+		if size <= 100000 {
+			sum += size
+		}
+	}
+
+	return sum
 }
